22dde0a783d6de88d208df67a1807eca0b7220b1
Jim Kyle <jim@jwkyle.com>
2017-12-15 16:29:25 -0600

    add setHiddenFields function

--- assets/main.js
+++ assets/main.js
@@ -6,4 +6,11 @@ function guess() {
     //add functionality to guess function here
 }
 
+function setHiddenFields() {
+    answer.value = Math.floor(Math.random()*10000).toString;
+    while(answer.value.length < 4) {
+      answer.vale = "0" + answer.value;
+    }
+    attempt.value = "0"
+}
 //implement new functions here
